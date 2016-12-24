09560bf5d29ac93fd4d145932cd497d3706e2b65
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2016-12-24 19:01:10 +0300

    feat(app): add api

--- faker_online/app.py
+++ faker_online/app.py
@@ -1,11 +1,28 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
+from flask.json import dumps
+from faker import Faker
 
 from faker_online.config import config
 
 app = Flask(__name__, static_folder=config.STATIC_FOLDER)
 app.config.from_object(config)
 app.debug = config.DEBUG
+
+
+def call(func):
+    return func()
+
+
+def serialize(obj):
+    return str(obj)
+
+
+@app.route('/api/<string:page>')
+def api(page):
+    fake = Faker(locale=request.args.get('locale', None))
+    result = {'response': call(getattr(fake, page))}
+    return dumps(result, default=serialize)
 
 
 @app.route('/')
